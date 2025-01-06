e8c05a3fbfda4310e622367b11262b84c105a462
Gurpreet Singh <gurupreetsingh9073@gmail.com>
2025-01-06 19:43:44 +0530

    [IMP] sickware: add the cart page and make add to cart work

--- sickwear/product/urls.py
+++ sickwear/product/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('category/<slug:slug>/', views.category, name='category'),
-    path('product-detail/<slug:slug>/', views.product_detail, name='product_detail')
+    path('product-detail/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('add-to-cart', views.add_to_cart, name='add_to_cart'),
+    path('view-cart', views.view_cart, name='view_cart'),
 ]
