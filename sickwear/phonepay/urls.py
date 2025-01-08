b04ded2afb8df7afbfe09b723d696f6754d1c8cb
kamlesh pathekar <kamleshdns99@gmail.com>
2025-01-08 14:38:38 +0530

    Update django.yml

--- sickwear/phonepay/urls.py
+++ sickwear/phonepay/urls.py
@@ -1,7 +1,8 @@
-"""sickwear URL Configuration
+"""
+URL configuration for PhonePayDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,16 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('user/', include('user.urls')),
-    path('product/', include('product.urls')),
-    path('phonepay/', include('phonepay.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index),
+    path('pay/', views.pay, name="pay"),
+    path('return-to-me/', views.payment_return, name="return-to-me"),
+]
