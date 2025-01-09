5e00f54af68c2088f7ac1df446cc9aab415e95aa
Gurpreet Singh <gurupreetsingh9073@gmail.com>
2025-01-09 17:54:15 +0530

    [FIX] sickwear: improve the login exp now user can login with email and username both

--- sickwear/user/views.py
+++ sickwear/user/views.py
@@ -8,9 +8,17 @@ User = get_user_model()
 
 def signin(request):
     if request.method == 'POST':
-        email = request.POST['email']
+        login_input = request.POST['email']  # Can be email or username
         password = request.POST['password']
-        user = authenticate(username=email, password=password)
+        # Check if login input is an email
+        try:
+            user = User.objects.get(email=login_input)
+            username = user.username
+        except User.DoesNotExist:
+            username = login_input  # Assume it's a username if not an email
+        # Authenticate with the resolved username
+        user = authenticate(username=username, password=password)
+
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in.')
@@ -22,22 +30,30 @@ def signin(request):
 
 def signup(request):
     if request.method == 'POST':
-        name = request.POST['username']
+        name = request.POST['name']
+        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirmpassword']
         phone = request.POST['phone']
         address = request.POST['address']
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return redirect('signup')
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not match.')
+            return redirect('signup')
         if User.objects.filter(email=email).exists():
             messages.error(request, 'Email already exists.')
             return redirect('signup')
-        new_user = User.objects.create_user(username=email, email=email, password=password)
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.name = name
         new_user.phone_number = phone
         new_user.address = address
         new_user.save()
         messages.success(request, 'You have successfully signed up.')
         #log user in and redirect to settings page
-        user_login = authenticate(username=email, password=password)
+        user_login = authenticate(username=username, password=password)
         login(request, user_login)
         return redirect('/')
 
