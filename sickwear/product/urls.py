f4a2dd7aed9be7e604725bf43dcd3e263ef0ddd2
Gurpreet Singh <gurupreetsingh9073@gmail.com>
2025-01-23 18:35:02 +0530

    [IMP] sickwear: create js so we can update the cart without reloadiing or rerendering

--- sickwear/product/urls.py
+++ sickwear/product/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('wish-list', views.wish_list, name='wish_list'),
     # path('add-to-wishlist', views.add_to_wishlist, name='add_to_wishlist'),
     # path('remove-from-cart', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update/', views.update_cart_item_quantity, name='update_cart_item'),
+    path('cart/remove/', views.remove_cart_item, name='remove_cart_item'),
     path('add-or-edit-address', views.add_or_edit_address, name='add_or_edit_address'),
 ]
